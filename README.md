9a2c6a4ff03590f2bf3c6f5b267e1f24c32dab14
YoAquinjs <sebascertuche@gmail.com>
2024-08-29 13:12:01 -0600

    refactor veectors.py generator

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # Vector Sum CLI
---------------
 
-Vector sum script using trigonometry (cosine law) and analytic geometry, must provide always at least two vectors, two uses:
+Vector sum program using trigonometry (cosine law) and analytic geometry, must provide always at least two vectors, two uses:
 - Passing arguments via command line, insert number of vectors to sum and fill each vector magnitude and angle, args:
   **isRads** angle values input in radians *rads* or degrees *grads*
   **floatingPrecision** floating point decimal place, positive integer or 0
 - Passing arguments by a text file, format data like "magnitude1 angle1 magnitude2 angle2..." separated by spaces
   **filename** file name containing vectors data
-  
-for generating a file with vectors
-```
-python vectors.py vectors.txt numberOfVectors
+
+------------
+
+The python script vector-gen serve to generate random vectors to a file for testing,
+use -h for details on usage
+```sh
+# usage: vector-gen.py [-h] [-f FILENAME] [-c VECTOR_COUNT] [-d]
+python vector-gen.py -f vectors.txt -c 1000
 ```
---
+++ vector-gen.py
@@ -0,0 +1,35 @@
+"""Generates a file with random vectors"""
+
+import argparse
+import math
+from random import uniform
+
+description = "Script for generating a file with random 2D vectors"
+parser = argparse.ArgumentParser(description=description)
+
+parser.add_argument("-f", "--filename", type=str, default="vectors.txt",
+                    help="The name of the output file")
+parser.add_argument("-c", "--vector-count", type=int, default=100,
+                    help="The number of vectors to store")
+parser.add_argument("-d", "--degrees", action="store_true", default=False,
+                    help="Use degrees instead of radians")
+
+def main() -> None:
+
+    args = parser.parse_args()
+
+    random_degree = lambda: round(uniform(1, 360), 2)
+    random_rad = lambda: round(uniform(0, math.pi*2), 4)
+    gen_direction = random_degree if args.degrees else random_rad
+
+    vectors = []
+    for _ in range(args.vector_count):
+        magnitude = round(uniform(1, 100), 2)
+        direction = gen_direction()
+        vectors.append(f"{magnitude}\t{direction}")
+
+    with open(args.filename, "w") as file:
+        file.write("\n".join(vectors))
+
+if __name__ == "__main__":
+    main()
